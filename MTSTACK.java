95931dfa5eab39b208b3c1bc76858101070f88d4
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-10-18 13:44:32 -0400

    added top

--- MTSTACK.java
+++ MTSTACK.java
@@ -5,6 +5,13 @@ public class MTSTACK<X> implements ISTACK<X>{
 	public X pop() throws Exception{
 		throw new Exception("Method first applied to an empty stack");}
 	public MTSTACK<X> push (X v){return(new MTSTACK<X>(v,this));}
+	public MTSTACK<X> reverse() {return this;}
+	public X top() throws Exception{
+		throw new Exception("Method first applied to an empty stack");}
+	
+	
+
+	
 	
 	
 }
