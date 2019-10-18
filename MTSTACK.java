315a09b0523f9143a30dfc23e1611c47aaa61925
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-10-18 14:43:55 -0400

    Update MTSTACK.java

--- MTSTACK.java
+++ MTSTACK.java
@@ -3,11 +3,16 @@ public class MTSTACK<X> implements ISTACK<X>{
 	
 	public MTSTACK() {};  
 	public X pop() throws Exception{
-		throw new Exception("Method first applied to an empty stack");}
-	public MTSTACK<X> push (X v){return(new MTSTACK<X>(v,this));}
-	public MTSTACK<X> reverse() {return this;}
+		throw new Exception("Method pop applied to an empty stack");}
+	
+	public MTSTACK<X> push (X v){return this;}
+	
 	public X top() throws Exception{
 		throw new Exception("Method first applied to an empty stack");}
+	
+	public boolean emptyStack() {return true;}
+	
+	public MTSTACK<X> reverse() {return this;}
 	
 	
 
