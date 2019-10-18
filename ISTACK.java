4f1b5653043fb0e72ba12105c7b650252ef6598c
Emanoid <56281174+Emanoid@users.noreply.github.com>
2019-10-18 18:02:37 -0400

    Update ISTACK.java

--- ISTACK.java
+++ ISTACK.java
@@ -1,13 +1,18 @@
+package Stack;
 
 public interface ISTACK<X> {
 	public ISTACK<X> push(X v);
 	//adds top to stack
-	public ISTACK<X> top();
+	public X top() throws Exception;
 	//gets top of Stack
-	public ISTACK<X> pop();
+	public ISTACK<X> pop() throws Exception;
 	//removes the top from the stack
 	public boolean emptyStack();
 	//Purpose: to check if stack is empty
 	public ISTACK<X> reverse();
-}
+	//To reverse the stack
+	public ISTACK<X> stackrest() throws Exception;
+	//To obtain all the elements of the stack besides top
+	public String ToString();
 
+}
