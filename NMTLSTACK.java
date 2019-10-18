d1c230f755d12ec276931a24b91bc026e8247c83
bgmrz <45674874+bgmrz@users.noreply.github.com>
2019-10-18 14:14:56 -0400

    Update NMTLSTACK.java

--- NMTLSTACK.java
+++ NMTLSTACK.java
@@ -1,4 +1,5 @@
 
+
 public class NMTLSTACK<X> implements ISTACK<X> {
 	private X car;
 	private StackIList<X> cdr;
@@ -13,6 +14,14 @@ public class NMTLSTACK<X> implements ISTACK<X> {
 	public X top() throws Exception{return car;}
 	public X rest() throws Exception{return cdr;}
 	
+	private NMTLSTACK<X> revhelper(ISTACK<X> l, ISTACK<X>res)
+	//ACCUM INV: reverse(this) == append(reverse(l), res)
+	{try {if(l.isEmpty()) {return res;}
+	else{return (revhelper(l.rest(), res.cons(l.first())));}}
+	catch(Exception e) {
+		System.out.println("Error StackNMTLIST append: "+ e.getMessage());
+		return this;}	}
 	
-
+	public ISTACK<X> reverse() {return(revhelper(this, new NMTLSTACK<X>()));}
+	
 }
